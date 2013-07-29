5a63a4fbeff1147e5450f8a1cad8ee05e3a34ba3
mitcc <mitcc@qq.com>
2013-07-30 00:38:34 +0800

    update Two Sum from O(n^2) to O(n)

--- leetcode/TwoSum.java
+++ leetcode/TwoSum.java
@@ -1,7 +1,7 @@
 public class TowSum {
     public int[] twoSum(int[] numbers, int target) {
-        int[] result = new int[2];
-        for(int i = 0; i < numbers.length - 1; i++) {
+        /*int[] result = new int[2];
+        for(int i = 0; i < numbers.length - 1; i++) {//from 0 to length - 2;
             for(int j = i + 1; j < numbers.length; j++) {
                 if(numbers[i] + numbers[j] == target) {
                     result[0] = i + 1; 
@@ -10,6 +10,22 @@ public class TowSum {
                 }
             }
         }
-        return null;
+        return result;*/
+
+        int[] result = new int[2];
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for(int i = 0; i < numbers.length; i++) {//from 0 to length - 1;   
+            if(map.containsKey(numbers[i])) {
+                int index = map.get(numbers[i]);
+                if(index < i) {
+                    result[0] = index + 1;
+                    result[1] = i + 1;
+                    break;
+                }
+            }
+            if(!map.containsKey(numbers[i]))
+                map.put(target - numbers[i], i);
+        }
+        return result;
     }
 }
