da837cbe147fc353984dccc1dba69e027385a105
mitcc <mitcc@qq.com>
2013-07-31 23:03:28 +0800

    update LongestSubstringWithoutRepeatinCharacters.java

--- leetcode/LongestSubstringWithoutRepeatingCharacters.java
+++ leetcode/LongestSubstringWithoutRepeatingCharacters.java
@@ -12,25 +12,44 @@ public class LongestSubstringWithoutRepeatingCharacters {
 	public int lengthOfLongestSubstring(String s) {
         // Start typing your Java solution below
         // DO NOT write main() function
+//		int len = s.length();
+//		int max = 0;
+//		ArrayList<Character> list ;
+//		for(int i = 0; i < len; i++) {
+//			list = new ArrayList<Character>();
+//			int j;
+//			for(j = i; j < len; j++) {
+//				char c = s.charAt(j);
+//				if(list.contains(c))
+//					break;
+//				else
+//					list.add(c);
+//			}
+//			max = Math.max(max, j - i);
+//		}
+//		return max;
 		int len = s.length();
 		int max = 0;
-		ArrayList<Character> list ;
-		for(int i = 0; i < len; i++) {
-			list = new ArrayList<Character>();
-			int j;
-			for(j = i; j < len; j++) {
-				char c = s.charAt(j);
-				if(list.contains(c))
-					break;
-				else
-					list.add(c);
+		boolean[] occur = new boolean[256];
+		int i = 0, j = 0; 
+		while(j < len) {
+			if(occur[s.charAt(j)]) {
+				max = Math.max(max, j - i);
+				while(s.charAt(i) != s.charAt(j)) {
+					occur[s.charAt(i)] = false;
+					i++;
+				}
+				i++;
+				j++;
+			} else {
+				occur[s.charAt(j)] = true;
+				j++;
 			}
-			max = Math.max(max, j - i);
 		}
-		return max;
+		return Math.max(max, len - i);
     }
 	public static void main(String[] args) {
-		String s = "abcddef";
+		String s = "abcdefghglmknijpq";
 		System.out.println(new LongestSubstringWithoutRepeatingCharacters().lengthOfLongestSubstring(s));
 	}
 }
