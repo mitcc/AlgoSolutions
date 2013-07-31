379ee5f9a5e48789618e2ea0a0224ca8a3c89688
mitcc <mitcc@qq.com>
2013-08-01 01:07:15 +0800

    update PalindromeNumber.java

--- leetcode/PalindromeNumber.java
+++ leetcode/PalindromeNumber.java
@@ -14,5 +14,17 @@
 package info.mitcc.leetcode;
 
 public class PalindromeNumber {
-
+	public boolean isPalindrome(int x) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+		if(x < 0) 
+			return false;
+        int rev = 0, digit, temp = x;
+        while(temp != 0) {
+        	digit = temp % 10;
+        	rev = 10 * rev + digit;
+        	temp /= 10;
+        }
+        return rev == x;
+    }
 }
