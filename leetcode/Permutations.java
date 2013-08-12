c288fa40b56eb4adfca0f931779fce53889494b3
Fan Sun <mitcc@qq.com>
2013-08-12 14:33:09 +0800

    Create Permutations.java

---
+++ leetcode/Permutations.java
@@ -0,0 +1,16 @@
+/*
+ * Given a collection of numbers, return all possible permutations.
+
+ * For example,
+ * [1,2,3] have the following permutations:
+ * [1,2,3], [1,3,2], [2,1,3], [2,3,1], [3,1,2], and [3,2,1].
+ */
+package info.mitcc.leetcode;
+
+import java.util.*;
+
+public class Permutations {
+  public ArrayList<ArrayList<Integer>> permute(int[] num) {
+        
+    }
+}
