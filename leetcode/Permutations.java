2f0af3ced3132091288b4ec84b2ff0f4d32bb136
Fan Sun <mitcc@qq.com>
2013-08-13 15:31:02 +0800

    Update Permutations.java

--- leetcode/Permutations.java
+++ leetcode/Permutations.java
@@ -11,6 +11,21 @@ import java.util.*;
 
 public class Permutations {
   public ArrayList<ArrayList<Integer>> permute(int[] num) {
-        
+		ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+		ArrayList<Integer> item = new ArrayList<Integer>();
+		item.add(num[0]);
+		result.add(item);
+		for(int i = 1; i < num.length; i++) {
+			ArrayList<ArrayList<Integer>> list = new ArrayList<ArrayList<Integer>>();
+			for(int j = 0; j < result.size(); j++) {
+				for(int k = 0; k < result.get(j).size() + 1; k++) {
+					ArrayList<Integer> temp = new ArrayList<Integer>(result.get(j));
+					temp.add(k, num[i]);
+					list.add(temp);
+				}
+			}
+			result = list;
+		}
+		return result;
     }
 }
