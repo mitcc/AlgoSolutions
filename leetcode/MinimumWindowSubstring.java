2bd8428edb2051166582828f1cfef174947c53ca
mitcc <mitcc@qq.com>
2013-08-20 15:51:03 +0800

    update MinimumWindowSubstring.java

--- leetcode/MinimumWindowSubstring.java
+++ leetcode/MinimumWindowSubstring.java
@@ -18,23 +18,38 @@ import java.util.*;
 
 public class MinimumWindowSubstring {
 	public String minWindow(String S, String T) {
-        String result = "";
-        HashSet<Character> setT = charSet(T);
-        int i = 0, j = S.length() - 1;
-        HashSet<Character> temp = charSet(S);
-        while(temp.containsAll(setT)) {
-        	
-        }
-        
-        
-        
-        return result;
-    }
-	
-	public HashSet<Character> charSet(String s) {
-		HashSet<Character> set = new HashSet<Character>();
-		for(int i = 0; i < s.length(); i++)
-			set.add(s.charAt(i));
-		return set;
+		String result = "";
+		int sLen = S.length();
+		int tLen = T.length();
+		int count = 0;
+		int minWindowLen = Integer.MAX_VALUE;	
+		int[] hasFound = new int[256];
+		int[] needToFound = new int[256];
+		Arrays.fill(hasFound, 0);
+		Arrays.fill(needToFound, 0);
+		for(int i = 0; i < tLen; i++)
+			needToFound[T.charAt(i)]++;
+		
+		for(int begin = 0, end = 0; end < sLen; end++) {
+			if(needToFound[S.charAt(end)] == 0)
+				continue;
+			hasFound[S.charAt(end)]++;
+			if(hasFound[S.charAt(end)] <= needToFound[S.charAt(end)])
+				count++;
+			
+			if(count == tLen) {
+				while(needToFound[S.charAt(begin)] == 0 || hasFound[S.charAt(begin)] > needToFound[S.charAt(begin)]) {
+					if(hasFound[S.charAt(begin)] > needToFound[S.charAt(begin)])
+						hasFound[S.charAt(begin)]--;
+					begin++;
+				}
+				int windowLen = end - begin + 1;
+				if(windowLen < minWindowLen) {
+					minWindowLen = windowLen;
+					result = S.substring(begin, end + 1);
+				}
+			}
+		}
+		return result;
 	}
 }
