0b724acc9ff73f51f46f083a13ad00ca12bdd0bd
mitcc <mitcc@qq.com>
2013-08-21 20:08:12 +0800

    update LongestSubstringWithoutRepeatingCharacters.java

--- leetcode/LongestSubstringWithoutRepeatingCharacters.java
+++ leetcode/LongestSubstringWithoutRepeatingCharacters.java
@@ -12,22 +12,23 @@ public class LongestSubstringWithoutRepeatingCharacters {
 	public int lengthOfLongestSubstring(String s) {
         // Start typing your Java solution below
         // DO NOT write main() function
-//		int len = s.length();
-//		int max = 0;
-//		ArrayList<Character> list ;
-//		for(int i = 0; i < len; i++) {
-//			list = new ArrayList<Character>();
-//			int j;
-//			for(j = i; j < len; j++) {
-//				char c = s.charAt(j);
-//				if(list.contains(c))
-//					break;
-//				else
-//					list.add(c);
-//			}
-//			max = Math.max(max, j - i);
-//		}
-//		return max;
+/*		int len = s.length();
+		int max = 0;
+		ArrayList<Character> list ;
+		for(int i = 0; i < len; i++) {
+			list = new ArrayList<Character>();
+			int j;
+			for(j = i; j < len; j++) {
+				char c = s.charAt(j);
+				if(list.contains(c))
+					break;
+				else
+					list.add(c);
+			}
+			max = Math.max(max, j - i);
+		}
+		return max;*/
+		
 		int len = s.length();
 		int max = 0;
 		boolean[] occur = new boolean[256];
