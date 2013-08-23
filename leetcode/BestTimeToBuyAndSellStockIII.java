6bf5624316e166db2fb7e4fda5e7f4001a38e076
mitcc <mitcc@qq.com>
2013-08-23 20:08:16 +0800

    add BestTimeToBuyAndSellStockIII.java

---
+++ leetcode/BestTimeToBuyAndSellStockIII.java
@@ -0,0 +1,43 @@
+/*
+ * Say you have an array for which the ith element is the price of a given stock on day i.
+
+ * Design an algorithm to find the maximum profit. You may complete at most two transactions.
+
+ * Note:
+ * You may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again). 
+ */
+package info.mitcc.leetcode;
+
+public class BestTimeToBuyAndSellStockIII {
+	public int maxProfit(int[] prices) {
+		if(prices.length == 0)
+			return 0;
+		int[] leftMax = new int[prices.length];
+		int[] rightMax = new int[prices.length];
+		int result = 0;
+		
+		int min = prices[0];
+		leftMax[0] = 0;
+		for(int i = 1; i < prices.length; i++) {
+			min = Math.min(min, prices[i]);
+			leftMax[i] = Math.min(leftMax[i], prices[i] - min);
+		}
+		
+		int max = prices[prices.length - 1];
+		rightMax[prices.length - 1] = 0;
+		for(int i = prices.length - 2; i >= 0; i--) {
+			max = Math.max(max, prices[i]);
+			rightMax[i] = Math.max(rightMax[i], max - prices[i]);
+		}
+		
+		for(int i = 0; i < prices.length; i++) {
+			result = Math.max(result, leftMax[i] + rightMax[i]);
+		}
+		return result;
+	}
+	
+	public static void main(String[] args) {
+		int[] test = {1, 3, 2, 7, 6, 8, 9};
+		System.out.println(new BestTimeToBuyAndSellStockIII().maxProfit(test));
+	}
+}
