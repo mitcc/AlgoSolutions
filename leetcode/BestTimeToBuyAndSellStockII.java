daeee649d98f6e2f1884cc1b35cc0029c100548e
mitcc <mitcc@qq.com>
2013-08-24 16:42:46 +0800

    add BestTimeToBuyAndSellStockII.java

---
+++ leetcode/BestTimeToBuyAndSellStockII.java
@@ -0,0 +1,20 @@
+/*
+ * Say you have an array for which the ith element is the price of a given stock on day i.
+
+ * Design an algorithm to find the maximum profit. You may complete as many transactions as 
+ * you like (ie, buy one and sell one share of the stock multiple times). However, you may 
+ * not engage in multiple transactions at the same time (ie, you must sell the stock before 
+ * you buy again).
+ */
+package info.mitcc.leetcode;
+
+public class BestTimeToBuyAndSellStockII {
+	public int maxProfit(int[] prices) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        int result = 0; 
+        
+        
+        return result;
+    }
+}
