69403e4addcbafe6f828860226acf87aad181f24
mitcc <mitcc@qq.com>
2013-08-24 16:45:34 +0800

    update BestTimeToBuyAndSellStockII.java

--- leetcode/BestTimeToBuyAndSellStockII.java
+++ leetcode/BestTimeToBuyAndSellStockII.java
@@ -10,11 +10,11 @@ package info.mitcc.leetcode;
 
 public class BestTimeToBuyAndSellStockII {
 	public int maxProfit(int[] prices) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        int result = 0; 
-        
-        
-        return result;
+        int result = 0;
+        for(int i = 1; i < prices.length; i++) {
+            if(prices[i] > prices[i - 1])
+                result += prices[i] - prices[i - 1];
+        }
+        return result;  
     }
 }
