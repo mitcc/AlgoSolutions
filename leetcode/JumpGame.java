6bf30934fa73180bcd1e7436b0611b711cc5a6e2
mitcc <mitcc@qq.com>
2013-08-25 19:13:53 +0800

    update JumpGameII.java

--- leetcode/JumpGame.java
+++ leetcode/JumpGame.java
@@ -1,6 +1,20 @@
+/*
+ * Given an array of non-negative integers, you are initially positioned at the first index of the array.
+
+ * Each element in the array represents your maximum jump length at that position.
+
+ * Determine if you are able to reach the last index.
+
+ * For example:
+ * A = [2,3,1,1,4], return true.
+
+ * A = [3,2,1,0,4], return false.
+ */
+package info.mitcc.leetcode;
+
 public class JumpGame {
-    public boolean canJump(int[] A) {
-        int maxIndex = 0;
+	public boolean canJump(int[] A) {
+		int maxIndex = 0;
         for(int i = 0; i < A.length; i++) {
             if(maxIndex >= i) {
                 maxIndex = Math.max(maxIndex, i + A[i]);
@@ -11,5 +25,5 @@ public class JumpGame {
             }
         }
         return true;
-    }
+	}
 }
--- leetcode/JumpGameII.java
+++ leetcode/JumpGameII.java
@@ -1,5 +1,19 @@
+/*
+ * Given an array of non-negative integers, you are initially positioned at the first index of the array.
+
+ * Each element in the array represents your maximum jump length at that position.
+
+ * Your goal is to reach the last index in the minimum number of jumps.
+
+ * For example:
+ * Given array A = [2,3,1,1,4]
+
+ * The minimum number of jumps to reach the last index is 2. (Jump 1 step from index 0 to 1, then 3 steps to the last index.)
+ */
+package info.mitcc.leetcode;
+
 public class JumpGameII {
-    public int jump(int[] A) {
+	public int jump(int[] A) {
         int[] dp = new int[A.length];
         dp[0] = 0;
         for(int i = 1; i < A.length; i++) {
@@ -9,7 +23,16 @@ public class JumpGameII {
                     break;
                 }
             }
+            /* to avoid the test arrays can not jump to the last index. If returns 0 : NO; else : Yes
+             * if(dp[i] == 0)
+             * 	   break;
+             */
         }
         return dp[A.length - 1];        
     }
+	
+	public static void main(String[] args) {
+		int[] test = {0, 2, 1};
+		System.out.println(new JumpGameII().jump(test));
+	}
 }
