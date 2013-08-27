99bcc745a0e22cc12b7b3aa0d08c66c687c470d1
Fan Sun <mitcc@qq.com>
2013-08-27 15:09:50 +0800

    Update LengthOfLastWord.java

--- leetcode/LengthOfLastWord.java
+++ leetcode/LengthOfLastWord.java
@@ -15,17 +15,17 @@ package info.mitcc.leetcode;
 public class LengthOfLastWord {
 	public int lengthOfLastWord(String s) {
 		if(s.length() == 0)
-            return 0;
-        int end = s.length() - 1;
-        while(end >= 0 && s.charAt(end) == ' ') {
-            end--;
-        }
-        int start = end;
-        while(start >= 0 && s.charAt(start) != ' ') {
-            start--;
-        }
-        return end - start; 
-    }
+            		return 0;
+        	int end = s.length() - 1;
+        	while(end >= 0 && s.charAt(end) == ' ') {
+            		end--;
+        	}
+        	int start = end;
+        	while(start >= 0 && s.charAt(start) != ' ') {
+            		start--;
+        	}
+        	return end - start; 
+    	}
 	
 /*	public int lengthOfLastWord(String s) {
 		if(s.length() == 0)
