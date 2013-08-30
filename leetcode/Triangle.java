837c9f46117a0dbc395d569c4dc6f639a201c23a
mitcc <mitcc@qq.com>
2013-08-30 11:21:13 +0800

    update Triangle.java

--- leetcode/Triangle.java
+++ leetcode/Triangle.java
@@ -1,5 +1,6 @@
 /*
- * Given a triangle, find the minimum path sum from top to bottom. Each step you may move to adjacent numbers on the row below.
+ * Given a triangle, find the minimum path sum from top to bottom. Each step you may move 
+ * to adjacent numbers on the row below.
 
  * For example, given the following triangle
 
@@ -11,7 +12,8 @@
  * ]
  * The minimum path sum from top to bottom is 11 (i.e., 2 + 3 + 5 + 1 = 11).
  * Note:
- * Bonus point if you are able to do this using only O(n) extra space, where n is the total number of rows in the triangle.
+ * Bonus point if you are able to do this using only O(n) extra space, where n is the total 
+ * number of rows in the triangle.
  */
 package info.mitcc.leetcode;
 
