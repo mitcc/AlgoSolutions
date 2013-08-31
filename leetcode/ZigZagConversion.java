e60ab376a2fb3f98b2d9d015886e5c71d0fda3c9
mitcc <mitcc@qq.com>
2013-08-31 12:24:01 +0800

    update ZigZagConversion.java

--- leetcode/ZigZagConversion.java
+++ leetcode/ZigZagConversion.java
@@ -1,3 +1,16 @@
+/*
+ * The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like 
+ * this: (you may want to display this pattern in a fixed font for better legibility)
+
+ * P   A   H   N
+ * A P L S I I G
+ * Y   I   R
+ * And then read line by line: "PAHNAPLSIIGYIR"
+ * Write the code that will take a string and make this conversion given a number of rows:
+
+ * string convert(string text, int nRows);
+ * convert("PAYPALISHIRING", 3) should return "PAHNAPLSIIGYIR". 
+ */
 public class ZigZagConversion {
     public String convert(String s, int nRows) {
         if(nRows == 1)
@@ -6,16 +19,15 @@ public class ZigZagConversion {
         int zigZagCount = s.length() / (2 * nRows - 2);
         String result = "";
         for(int i = 0; i < nRows; i++) {
-            for(int j = 0; j < zigZagCount; j++) {
-                if(i == 0 || i == nRows - 1)
+            for(int j = 0; j <= zigZagCount; j++) {
+                if((i == 0 || i == nRows - 1) && j * (2 * nRows - 2) + i < s.length())
                     result += s.charAt(j * (2 * nRows - 2) + i);
-                else
-                    result += s.charAt(j * (2 * nRows - 2) + i) + s.charAt(j * (2 * nRows - 2) + 2 * nRows - 2 - i);
+                else {
+                    if(j * (2 * nRows - 2) + i < s.length())
+                        result += s.charAt(j * (2 * nRows - 2) + i);
+                    if(j * (2 * nRows - 2) + 2 * nRows - 2 - i < s.length())
+                        result += s.charAt(j * (2 * nRows - 2) + 2 * nRows - 2 - i);
             }
-            if(zigZagCount * (2 * nRows - 2) + i < s.length())
-                result += s.charAt(zigZagCount * (2 * nRows - 2) + i);
-            if(zigZagCount * (2 * nRows - 2) + 2 * nRows - 2 - i < s.length())
-                result += s.charAt(zigZagCount * (2 * nRows - 2) + 2 * nRows - 2 - i);
         }
         return result;
     }
