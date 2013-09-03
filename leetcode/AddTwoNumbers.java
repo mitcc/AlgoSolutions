7cf45e5b5cff491fd8afa4b2fbf651c8ed449aa1
mitcc <mitcc@qq.com>
2013-09-03 11:25:38 +0800

    update AddTwoNumbers.java

--- leetcode/AddTwoNumbers.java
+++ leetcode/AddTwoNumbers.java
@@ -35,4 +35,26 @@ public class AddTwoNumbers {
         head.next = addTwoNumbersHelper((l1 == null ? null : l1.next), (l2 == null ? null : l2.next), (i + j + carry) / 10);
         return head;
     }
+
+/*  public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int carry = 0;
+        ListNode result = new ListNode(0);
+        ListNode cur1 = l1, cur2 = l2, head = result;
+        while(cur1 != null || cur2 != null) {
+            while(cur1 != null) {
+                carry += cur1.val;
+                cur1 = cur1.next;
+            }
+            while(cur2 != null) {
+                carry += cur2.val;
+                cur2 = cur2.next;
+            }
+            head.next = new ListNode(carry % 10);
+            head = head.next;
+            carry /= 10;
+        }
+        if(carry == 1)
+            head.next = new ListNode(carry);
+        return result.next;
+    }*/
 }
