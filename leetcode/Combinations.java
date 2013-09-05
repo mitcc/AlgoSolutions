f52395e651cb7d2e89dc2d036034091c791ee23f
mitcc <mitcc@qq.com>
2013-09-05 14:24:06 +0800

    add Combinations.java

---
+++ leetcode/Combinations.java
@@ -0,0 +1,38 @@
+/*
+ * Given two integers n and k, return all possible combinations of k numbers out of 1 ... n.
+
+ * For example,
+ * If n = 4 and k = 2, a solution is:
+
+ * [
+ *   [2,4],
+ *   [3,4],
+ *   [2,3],
+ *   [1,2],
+ *   [1,3],
+ *   [1,4],
+ * ]
+ */
+public class Combinations {
+    public ArrayList<ArrayList<Integer>> combine(int n, ink k) {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+        if(k == 0 || n < k)
+            return result;
+        if(k == 1) {
+            for(int i = 1; i <= n; i++){
+                ArrayList<Integer> temp = new ArrayList<Integer>();
+                temp.add(i);
+                result.add(temp);
+            }
+            return result;
+        } else {
+            for(int i = n; i >= k; i--) {
+                for(ArrayList<Integer> temp : combine(n - 1, k - 1)) {
+                    temp.add(i);
+                    result.add(temp);
+                    return result;
+                }
+            }
+        }
+    }
+}
