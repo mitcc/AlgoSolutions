4ffea9f42f43f9e8f6e115328d8e6da73070576e
mitcc <mitcc@qq.com>
2013-09-09 23:44:30 +0800

    update ReverseLinkedListII.java

--- leetcode/ReverseLinkedListII.java
+++ leetcode/ReverseLinkedListII.java
@@ -10,8 +10,8 @@
  * }
  */
 public class ReverseLinkedListII {
-    public ListNode reverseBetween(ListNode head, int m, int n) {
-        for(int i = 0; i < (n - m) / 2; i++) {
+/*  public ListNode reverseBetween(ListNode head, int m, int n) {
+        for(int i = 0; i <= (n - m) / 2; i++) {
             int curM = m + i;
             int curN = n - i;
             ListNode mNode = head;
@@ -28,6 +28,53 @@ public class ReverseLinkedListII {
             mNode.val = nNode.val;
             nNode.val = temp;
         }
+        return head;
+    }
+
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+        if(head == null)
+            return head;
+        ListNode runner = head, first = null;
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        while(n > 0) {
+            if(m == 1)
+                first = runner;
+            if(m <= 1)
+                list.add(runner.val);
+            m--;
+            n--;
+            runner = runner.next;
+        }
+        for(int i = list.size() - 1; i >= 0; i--) {
+            first.val = list.get(i);
+            first = first.next;
+        }
+        return head;
+    }
+*/
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+        if(m == n)
+            return head;
+        ListNode prev = null, current = head, next = null;
+        for(int i = 0; i < m - 1; i++) {
+            prev = current;
+            current = current.next;
+        }
+        ListNode low = prev, high = current;
+        prev = current;
+        current = current.next;
+        for(int i = m; i < n; i++) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        if(high != null)
+            high.next = current;
+        if(low != null)
+            low.next = prev;
+        else
+            head = prev;
         return head;
     }
 }
