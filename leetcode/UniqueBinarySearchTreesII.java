1b072261a70064034672b116d0cd79b0211bc066
mitcc <mitcc@qq.com>
2013-09-11 00:50:05 +0800

    add UniqueBinarySearchTreesII.java

--- leetcode/UniqueBinarySearchTreesII.java
+++ leetcode/UniqueBinarySearchTreesII.java
@@ -21,6 +21,25 @@
  */
 public class UniqueBinarySearchTreesII {
     public ArrayList<TreeNode> generateTrees(int n) {
-
+        return generateHelper(1, n);
+    }
+        
+    public ArrayList<TreeNode> generateHelper(int start, int end) {
+        ArrayList<TreeNode> list = new ArrayList<TreeNode>();
+        if(start > end) 
+            list.add(null);
+        for(int i = start; i <= end; i++) {
+            ArrayList<TreeNode> left = generateHelper(start, i - 1);
+            ArrayList<TreeNode> right = generateHelper(i + 1, end);
+            for(int j = 0; j < left.size(); j++) {
+                for(int k = 0; k < right.size(); k++) {
+                    TreeNode root = new TreeNode(i);
+                    root.left = left.get(j);
+                    root.right = right.get(k);
+                    list.add(root);
+                }
+            }
+        }
+        return list;
     }
 }
