3092c3fc9756e254baca08a05169b7bf6373c350
mitcc <mitcc@qq.com>
2013-09-12 02:33:04 +0800

    update DivideTwoIntegers.java

--- leetcode/DivideTwoIntegers.java
+++ leetcode/DivideTwoIntegers.java
@@ -4,13 +4,11 @@
 public class DivideTwoIntegers {
     public int divide(int dividend, int divisor) {
         boolean negative = false;
-        if(dividend < 0)
+        if(dividend < 0 && divisor > 0 || dividend >= 0 && divisor < 0)
             negative = !negative;
-        if(divisor < 0)
-            negative = !negative;
-        int result = 0;
         long a = Math.abs((long) dividend);
         long b = Math.abs((long) divisor);
+        int result = 0;
         while(a >= b) {
             long c = b;
             for(int i = 0; c <= a; i++, c = c << 1) {
