1af51932a550cebc69e10c5e6029e7ebbfc5a07e
mitcc <mitcc@qq.com>
2013-09-11 14:35:09 +0800

    update InterleavingString.java

--- leetcode/InterleavingString.java
+++ leetcode/InterleavingString.java
@@ -1,3 +1,14 @@
+/*
+ * Given s1, s2, s3, find whether s3 is formed by the interleaving of s1 and s2.
+
+ * For example,
+ * Given:
+ * s1 = "aabcc",
+ * s2 = "dbbca",
+
+ * When s3 = "aadbbcbcac", return true.
+ * When s3 = "aadbbbaccc", return false.
+ */
 public class InterleavingString {
     public boolean isInterleave(String s1, String s2, String s3) {
         if(s1.length() + s2.length() != s3.length())
