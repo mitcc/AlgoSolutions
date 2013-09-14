69e3f424aab2b722e142b096ad43c5c1ae4fc3f2
mitcc <mitcc@qq.com>
2013-09-15 00:56:48 +0800

    update LongestValidParentheses.java

--- leetcode/LongestValidParentheses.java
+++ leetcode/LongestValidParentheses.java
@@ -1,5 +1,6 @@
 /*
- * Given a string containing just the characters '(' and ')', find the length of the longest valid (well-formed) parentheses substring.
+ * Given a string containing just the characters '(' and ')', find the length of the longest valid (well-formed) 
+ * parentheses substring.
 
  * For "(()", the longest valid parentheses substring is "()", which has length = 2.
 
