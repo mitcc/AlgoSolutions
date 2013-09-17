15ade7f70d20c855e4b4dc65083f32785b209fc3
mitcc <mitcc@qq.com>
2013-09-17 12:12:00 +0800

    add BinaryTreeLevelOrderTraversal.java

---
+++ leetcode/BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,12 @@
+public class BinaryTreeLevelOrderTraversal {
+    public ArrayList<ArrayList<Integer>> levelOrder(TreeNode root) {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+        if(root == null)
+            return result;
+        ArrayList<TreeNode> cur =  new ArrayList<TreeNode>();
+        cur.add(root);
+        while(!cur.isEmpty()) {
+            
+        }
+    }
+}
