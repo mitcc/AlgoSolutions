04b6cc5f646363cfdfee71b2ee098b322757598a
mitcc <mitcc@qq.com>
2013-10-05 15:43:04 +0800

    update LongestPalindromicSubstring.java

--- leetcode/LongestPalindromicSubstring.java
+++ leetcode/LongestPalindromicSubstring.java
@@ -3,7 +3,8 @@
  * maximum length of S is 1000, and there exists one unique longest palindromic substring.
  */
 public class LongestPalindromicSubstring {
-    /** Below cost O(n^2) time and O(n^2) space. */
+/*
+    // Below cost O(n^2) time and O(n^2) space. 
     public String longestPalindrome(String s) {
         boolean[][] isPalin = new boolean[s.length()][s.length()];
         for(int i = 0; i < s.length(); i++) {
@@ -32,8 +33,8 @@ public class LongestPalindromicSubstring {
 
 
 
-    /** O(n^2) time and no space cost */
-    public String expandAroundCenter(String s, int i, int j) {
+    // O(n^2) time and no space cost 
+    public String expandAroundcenterenter(String s, int i, int j) {
         int left = i, right = j;
         while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
             left--;
@@ -47,13 +48,53 @@ public class LongestPalindromicSubstring {
             return "";
         String longest = "";
         for(int i = 0; i < s.length(); i++) {
-            String expand = expandAroundCenter(s, i, i);
+            String expand = expandAroundcenterenter(s, i, i);
             if(longest.length() < expand.length())
                 longest = expand;
-            expand = expandAroundCenter(s, i, i + 1);
+            expand = expandAroundcenterenter(s, i, i + 1);
             if(longest.length() < expand.length())
                 longest = expand;
         }
         return longest;
+    }
+*/
+
+
+    /** O(n) time and O(n) space cost. */
+    public String preProcess(String s) {
+        if(s.length() == 0)
+            return "#";
+        String result = "";
+        for(int i = 0; i < s.length(); i++) {
+            result += "#" + s.charAt(i);
+        }
+        result += "#";
+        return result;
+    }
+
+    public String longestPalindrome(String s) {
+        String T = preProcess(s);
+        int n = T.length();
+        int[] P = new int[n];
+        int center = 0, rightMost = 0;
+        for(int i = 1; i < n - 1; i++) {
+            int i_mirror = 2 * center - i;
+            P[i] = rightMost > i ? Math.min(rightMost - i, P[i_mirror]) : 0; 
+            while(i + 1 + P[i] < n && i >= 1 + P[i] && T.charAt(i + 1 + P[i]) == T.charAt(i - 1 - P[i])) {
+                P[i]++;
+            }
+            if(P[i] > center) {
+                center = i;
+                rightMost = i + center;
+            }
+        }
+        int maxLen = 0, beginAt = 0;
+        for(int i = 1; i < n - 1; i++) {
+            if(maxLen < P[i]) {
+                maxLen = P[i];
+                beginAt = i;
+            }
+        }
+        return s.substring((beginAt - maxLen) / 2, (beginAt - maxLen)/ 2 + maxLen);
     }
 }
