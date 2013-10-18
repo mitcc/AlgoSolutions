cb71835964fe8b7d66b0fdc91be3c330de1eb8ee
mitcc <mitcc@qq.com>
2013-10-18 12:11:03 +0800

    update Sqrt.java

--- leetcode/Sqrt.java
+++ leetcode/Sqrt.java
@@ -34,4 +34,25 @@ public class Sqrt {
         }
         return result;
     }
+
+
+
+    public int sqrt(int x) {
+        if(x < 0)
+            throw new IllegalArgumentException("negative input");
+        if(x == 0 || x == 1)
+            return x;
+        int start = 1;
+        int end = x / 2 + 1;
+        while(start <= end) {
+            int mid = (start + end) / 2;
+            if(x / mid == mid)
+                return mid;
+            else if(mid < x / mid)
+                start = mid + 1;
+            else
+                end = mid - 1;
+        }
+        return (start + end) / 2;
+    }
 }
