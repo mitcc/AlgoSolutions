8ddf218afd172b88f687933c323f1f4ad32dc1dc
mitcc <mitcc@qq.com>
2013-10-18 14:33:43 +0800

    update Sqrt.java

--- leetcode/Sqrt.java
+++ leetcode/Sqrt.java
@@ -2,20 +2,20 @@
  * Implement int sqrt(int x).
  * Compute and return the square root of x.
  */
-package info.mitcc.leetcode;
 
 public class Sqrt {
-	public int sqrt(int x) {
-		if(x < 100) {
-    		int i = 0; 
-			while(i * i <= x)
-				i++;
-			return i - 1;
-		}
-		String number = x + "";
-		int result = 0;
-		int tempDiff;
-		int nextProduct;
+    public int sqrt(int x) {
+        if(x < 100) {
+            int i = 0; 
+            while(i * i <= x) {
+                i++;
+            }
+            return i - 1;
+        }
+        String number = x + "";
+        int result = 0;
+        int tempDiff;
+        int nextProduct;
         int len = Integer.toString(x).length();
         int cursor = len % 2 == 1 ? 1 : 2;
         nextProduct = Integer.parseInt(number.substring(0, cursor));
@@ -24,13 +24,14 @@ public class Sqrt {
         tempDiff = nextProduct - divResult * divResult;
         
         for(; cursor <= number.length() - 2; cursor += 2) {
-        	nextProduct = 100 * tempDiff + Integer.parseInt(number.substring(cursor, cursor + 2));
-        	int i = 0; 
-        	while(i * i + result * 20 * i <= nextProduct)
-        		i++;
-        	i--; 	
-        	tempDiff = nextProduct - (i * i + result * 20 * i);
-        	result = result * 10 + i;
+            nextProduct = 100 * tempDiff + Integer.parseInt(number.substring(cursor, cursor + 2));
+            int i = 0; 
+            while(i * i + result * 20 * i <= nextProduct) {
+                i++;
+            }
+            i--; 	
+            tempDiff = nextProduct - (i * i + result * 20 * i);
+            result = result * 10 + i;
         }
         return result;
     }
@@ -54,5 +55,18 @@ public class Sqrt {
                 end = mid - 1;
         }
         return (start + end) / 2;
+    }
+
+
+    public int sqrt(int x) {
+        if(x < 0)
+            throw new IllegalArgumentException("negative input");
+        double result = x;
+        double last = 0;
+        while(last != result) {
+            last = result;
+            result = (result + x / result) / 2;
+        }
+        return (int)result;
     }
 }
