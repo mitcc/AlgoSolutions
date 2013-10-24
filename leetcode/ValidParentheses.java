5c5def95e1c17cea33b215194525adc1240e0863
mitcc <mitcc@qq.com>
2013-10-24 11:56:44 +0800

    update ValidParentheses.java;

--- leetcode/ValidParentheses.java
+++ leetcode/ValidParentheses.java
@@ -1,20 +1,38 @@
+/*
+ * Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+ * The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
+ * 
+ */
 public class ValidParentheses {
+/*    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{')
+                stack.push(s.charAt(i));
+            else if(stack.isEmpty())
+                return false;
+            else {
+                char pop = stack.pop();
+                if(pop == '(' && s.charAt(i) != ')' || pop == '[' && s.charAt(i) != ']'
+                        || pop == '{' && s.charAt(i) != '}')
+                    return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+*/
+
+
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<Character>();
         for(int i = 0; i < s.length(); i++) {
             if(s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{')
                 stack.push(s.charAt(i));
-            else {
-                if(stack.size() > 0) {
-                    char peek = stack.peek();
-                    if(!(s.charAt(i) == ')' && peek == '(' || s.charAt(i) == ']' && peek == '[' 
-                            || s.charAt(i) == '}' && peek == '{'))
-                        return false;
-                    else
-                        stack.pop();
-                }
-                else return false;  
-            }
+            else if(stack.isEmpty())
+                return false;
+            else if(s.charAt(i) - stack.pop() > 2)
+                return false;
         }
         return stack.isEmpty();
     }
