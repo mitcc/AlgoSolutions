10d9a81e1988ccc26d510b1e633639dfecbf6c9e
mitcc <mitcc@qq.com>
2013-10-26 17:49:09 +0800

    update ValidSukodu.java

--- leetcode/ValidSudoku.java
+++ leetcode/ValidSudoku.java
@@ -3,7 +3,7 @@
  * The Sudoku board could be partially filled, where empty cells are filled with the character '.'.
  */
 public class ValidSudoku {
-    public boolean isValidSudoku(char[][] board) {
+/*  public boolean isValidSudoku(char[][] board) {
         boolean[][] rows = new boolean[9][9];
         boolean[][] cols = new boolean[9][9];
         boolean[][] blocks = new boolean[9][9];
@@ -15,6 +15,27 @@ public class ValidSudoku {
                 if(rows[i][c] || cols[j][c] || blocks[i - i % 3 + j / 3][c])
                     return false;
                 rows[i][c] = cols[j][c] = blocks[i - i % 3 + j / 3][c] = true;
+            }
+        }
+        return true;
+    }
+*/
+
+    public boolean isValidSudoku(char[][] board) {
+        int row;
+        int[] col = new int[9];
+        int[] block = new int[9];
+        for(int i = 0; i < 9; i++) {
+            row = 0;
+            for(int j = 0; j < 9; j++) {
+                if(board[i][j] == '.')
+                    continue;
+                int bit = 1 << (board[i][j] - '1');
+                if((row & bit) != 0 || (col[j] & bit) != 0 || (block[i / 3 * 3 + j / 3] & bit) != 0)
+                    return false;
+                row |= bit;
+                col[j] |= bit;
+                block[i / 3 * 3 + j / 3] |= bit;
             }
         }
         return true;
