596911606efb78a9742cabc5505cea7c6449057a
mitcc <mitcc@qq.com>
2013-10-30 00:51:52 +0800

    update ValidNumber.java

--- leetcode/ValidNumber.java
+++ leetcode/ValidNumber.java
@@ -7,7 +7,8 @@
  * "abc" => false
  * "1 a" => false
  * "2e10" => true
- * Note: It is intended for the problem statement to be ambiguous. You should gather all requirements up front before implementing one.
+ * Note: It is intended for the problem statement to be ambiguous. You should gather all requirements up 
+ * front before implementing one.
  */
 public class ValidNumber {
     public boolean isNumber(String s) {
