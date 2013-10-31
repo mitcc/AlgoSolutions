a2c863bf35e50812ba0e5bfd83df75216c232f78
mitcc <mitcc@qq.com>
2013-10-31 22:43:54 +0800

    update WordBreak.java

--- leetcode/WordBreak.java
+++ leetcode/WordBreak.java
@@ -8,7 +8,27 @@
 
  * Return true because "leetcode" can be segmented as "leet code".
  */
-public class WordBreak {
+public class WordBrealen {
+/*  public boolean wordBreak(String s, Set<String> dict) {
+        boolean[][] dp = new boolean[s.length()][s.length() + 1];
+        for(int len = 1; len <= s.length(); len++) {
+            for(int i = 0; i <= s.length() - len; i++) {
+                if(dict.contains(s.substring(i, i + len))) 
+                    dp[i][len] = true;
+                else {
+                    for(int k = 1; k < len; k++) {
+                        if(dp[i][k] && dp[i + k][len - k]) {
+                            dp[i][len] = true;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        return dp[0][s.length()];
+    }
+
+
     public boolean wordBreak(String s, Set<String> dict) {
         boolean[] dp = new boolean[s.length() + 1];
         for(int i = 0; i < s.length(); i++) {
@@ -17,6 +37,20 @@ public class WordBreak {
             for(int j = s.length(); j > i; j--) {
                 dp[j] = dp[j] || dict.contains(s.substring(i, j));
             }  
+        }
+        return dp[s.length()];
+    }
+*/
+    public boolean wordBreak(String s, Set<String> dict) {
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+        for(int i = 0; i < s.length(); i++) {
+            if(dp[i]) {
+                for(int j = i + 1; j <= s.length(); j++) {
+                    if(dict.contains(s.substring(i, j)))
+                        dp[j] = true;
+                }
+            }
         }
         return dp[s.length()];
     }
