71001ee4b459dbc684bd5b7bddf0e4c6b6ddf3a6
mitcc <mitcc@qq.com>
2013-11-07 19:38:26 +0800

    update SameTree.java

--- leetcode/SameTree.java
+++ leetcode/SameTree.java
@@ -16,8 +16,8 @@ public class SameTree {
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if(p == null && q == null)
             return true;
-        if(p == null && q != null || p != null && q == null)
+        if(p == null && q != null || p != null && q == null || p.val != q.val)
             return false;
-        return (p.val == q.val) && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
