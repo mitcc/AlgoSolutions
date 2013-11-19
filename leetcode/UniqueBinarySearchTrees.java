ded1f31b6098c0b1b5696004d87938a8a7bbce8d
mitcc <mitcc@qq.com>
2013-11-19 16:25:40 +0800

    update UniqueBinarySearchTrees.java

--- leetcode/UniqueBinarySearchTrees.java
+++ leetcode/UniqueBinarySearchTrees.java
@@ -22,4 +22,14 @@ public class UniqueBinarySearchTrees {
         }
         return result[n];
     }
+
+/********************updated 2013/11/19 ***********************/
+    public int numTrees(int n) {
+        if(n == 0 || n == 1)
+            return 1;
+        int num = 0;
+        for(int i = 0; i < n; i++) 
+            num += numTrees(i) * numTrees(n - 1 - i);
+        return num;
+    }
 }
