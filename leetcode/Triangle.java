d580bacfd7b247b2aeb14dba4e9f57501c34f3ca
mitcc <mitcc@qq.com>
2013-11-20 17:01:07 +0800

    update Triangle.java

--- leetcode/Triangle.java
+++ leetcode/Triangle.java
@@ -15,24 +15,19 @@
  * Bonus point if you are able to do this using only O(n) extra space, where n is the total 
  * number of rows in the triangle.
  */
-package info.mitcc.leetcode;
-
-import java.util.ArrayList;
+import java.util.*;
 
 public class Triangle {
-	public int minimumTotal(ArrayList<ArrayList<Integer>> triangle) {
-
+    public int minimumTotal(ArrayList<ArrayList<Integer>> triangle) {
         int row = triangle.size();
-        int col = triangle.get(row - 1).size();
         int[][] dp = new int[row][];
+        for(int i = 0; i < row; i++) 
+            dp[i] = new int[i + 1];
         for(int i = 0; i < row; i++)
-        	dp[i] = new int[i + 1];
-        for(int i = 0; i < col; i++)
-        	dp[row - 1][i] = triangle.get(row - 1).get(i);
+            dp[row - 1][i] = triangle.get(row - 1).get(i);
         for(int i = row - 2; i >= 0; i--) {
-        	for(int j = 0; j <= i; j++) {
-        		dp[i][j] = Math.min(dp[i + 1][j], dp[i + 1][j + 1]) + triangle.get(i).get(j);
-        	}
+            for(int j = 0; j <= i; j++)
+                dp[i][j] = triangle.get(i).get(j) + Math.min(dp[i + 1][j], dp[i + 1][j + 1]);
         }
         return dp[0][0];
     }
