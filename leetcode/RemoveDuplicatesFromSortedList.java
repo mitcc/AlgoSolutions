9f73c12458e8b408673a650d7cd8885cceb996d3
mitcc <mitcc@qq.com>
2013-11-24 16:40:00 +0800

    update

--- leetcode/RemoveDuplicatesFromSortedList.java
+++ leetcode/RemoveDuplicatesFromSortedList.java
@@ -6,37 +6,28 @@
  * Given 1->1->2->3->3, return 1->2->3.
  * 
  */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
 public class RemoveDuplicatesFromSortedList {
-/*   public ListNode deleteDuplicates(ListNode head) {
-        if(head == null)
-            return head;
-        int value = head.val;
-        ListNode cursor = head.next;
-        ListNode prev = head;
-        while(cursor != null) {
-            if(value == cursor.val) {
-                prev.next = cursor.next;
-                cursor = cursor.next;    
-            } else {
-                value = cursor.val;
-                prev = prev.next;
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head != null) {
+            ListNode prev = head, cursor = head.next;
+            while(cursor != null) {
+                if(prev.val == cursor.val) 
+                    prev.next = cursor.next;
+                 else 
+                    prev = cursor;
                 cursor = cursor.next;
             }
-        }
-        return head;
-    } */
-
-    public ListNode deleteDuplicates(ListNode head) {
-        if(head == null)
-            return head;
-        ListNode cursor = head.next;
-        ListNode prev = head;
-        while(cursor != null) {
-            if(cursor.val == prev.val)
-                prev.next = cursor.next;
-            else
-                prev = cursor;
-            cursor = cursor.next;
         }
         return head;
     }
