3ec48e790f0f6b4e0d3dc38134b2d29f531b17f4
mitcc <mitcc@qq.com>
2013-11-29 17:34:48 +0800

    update Permutations.java

--- leetcode/Permutations.java
+++ leetcode/Permutations.java
@@ -5,32 +5,29 @@
  * [1,2,3] have the following permutations:
  * [1,2,3], [1,3,2], [2,1,3], [2,3,1], [3,1,2], and [3,2,1].
  */
-package info.mitcc.leetcode;
-
 import java.util.*;
 
 public class Permutations {
-	public ArrayList<ArrayList<Integer>> permute(int[] num) {
-		ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
-		ArrayList<Integer> item = new ArrayList<Integer>();
-		item.add(num[0]);
-		result.add(item);
-		for(int i = 1; i < num.length; i++) {
-			ArrayList<ArrayList<Integer>> list = new ArrayList<ArrayList<Integer>>();
-			for(int j = 0; j < result.size(); j++) {
-				for(int k = 0; k < result.get(j).size() + 1; k++) {
-					ArrayList<Integer> temp = new ArrayList<Integer>(result.get(j));
-					temp.add(k, num[i]);
-					list.add(temp);
-				}
-			}
-			result = list;
-		}
-		return result;
+/*************************** updated 2013/11/29 ***********************************/
+
+    public ArrayList<ArrayList<Integer>> permute(int[] num) {
+        ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
+        if(num.length > 0) {
+            ArrayList<Integer> init = new ArrayList<Integer>();
+            init.add(num[0]);
+            res.add(init);
+            for(int i = 1; i < num.length; i++) {
+                ArrayList<ArrayList<Integer>> newRes = new ArrayList<ArrayList<Integer>>();
+                for(int j = 0; j < res.size(); j++) {
+                    for(int k = 0; k <= res.get(j).size(); k++) {
+                        ArrayList<Integer> list = new ArrayList<Integer>(res.get(j));
+                        list.add(k, num[i]);
+                        newRes.add(list);
+                    }
+                }
+                res = newRes;
+            }
+        }
+        return res;
     }
-	
-	public static void main(String[] args) {
-		int[] num = {1, 2, 3};
-		System.out.println(new Permutations().permute(num));
-	}
 }
