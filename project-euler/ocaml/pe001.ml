18f1845824ea6a29ec57e4af34ef5c449fa2838a
mitcc <mitcc@qq.com>
2019-07-07 04:10:01 +0800

    pe001.ml

--- project-euler/ocaml/pe001.ml
+++ project-euler/ocaml/pe001.ml
@@ -1,12 +1,10 @@
 let add_3_5_mult acc x =
-  if (x mod 3) = 0 || (x mod 5) = 0 then acc + x
+  if x mod 3 = 0 || x mod 5 = 0 then acc + x
   else acc
-
 
 let rec fold f acc low high =
   if high < low then acc
   else fold f (f acc low) (low + 1) high
 
 let () =
-  Format.printf "%d" (fold add_3_5_mult 0 1 1000)
-
+  Format.printf "%d\n" (fold add_3_5_mult 0 1 1000)
