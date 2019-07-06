99269917717ea2a047f2af4253db1ebed295244e
mitcc <mitcc@qq.com>
2019-07-06 19:30:20 +0800

    pe005.ml

---
+++ project-euler/ocaml/pe005.ml
@@ -0,0 +1,9 @@
+let rec gcd a b = if b = 0 then a else gcd b (a mod b)
+
+let lcm a b = a * b / gcd a b
+
+let rec fold f acc low high =
+  if high < low then acc else fold f (f acc low) (low + 1) high
+
+let () =
+  Format.printf "%d" (fold lcm 1 1 20)
