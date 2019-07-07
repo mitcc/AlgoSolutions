520ef586573c9d7d337ebfa5fc1505e362cde2b7
mitcc <mitcc@qq.com>
2019-07-08 03:44:34 +0800

    pe005.ml

--- project-euler/ocaml/pe005.ml
+++ project-euler/ocaml/pe005.ml
@@ -1,3 +1,11 @@
+(*
+ * 2520 is the smallest number that can be divided by each of the numbers from
+ * 1 to 10 without any remainder.
+ *
+ * What is the smallest positive number that is evenly divisible by all of the
+ * numbers from 1 to 20?
+ *)
+
 let rec gcd a b = if b = 0 then a else gcd b (a mod b)
 
 let lcm a b = a * b / gcd a b
