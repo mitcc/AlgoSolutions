10d03ea8f1f717b339cd6128a25017ee3769ad3f
mitcc <mitcc@qq.com>
2019-07-09 00:43:49 +0800

    pe003.ml

---
+++ project-euler/ocaml/pe003.ml
@@ -0,0 +1,18 @@
+(*
+ * The prime factors of 13195 are 5, 7, 13 and 29.
+ *
+ * What is the largest prime factor of the number 600851475143 ?
+ *)
+
+let rec div_all n x = if n mod x <> 0 then n else div_all (n / x) x
+
+let max a b = if a > b then a else b
+
+let rec fold result low high =
+    if low >= high then max result high
+    else if high mod low = 0 then fold (max result low) (low + 1) (div_all high low)
+    else fold result (low + 1) high
+
+let () =
+ Format.printf "%d\n" (fold 1 2 600851475143)
+
