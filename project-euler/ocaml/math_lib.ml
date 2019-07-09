b36a6a0f3eee6515b93b0e9081ed4f95c4a9c03d
mitcc <mitcc@qq.com>
2019-07-09 13:08:58 +0800

    update math_lib

--- project-euler/ocaml/math_lib.ml
+++ project-euler/ocaml/math_lib.ml
@@ -1,1 +1,7 @@
 let max a b = if a > b then a else b
+
+let rec rev x =
+  let rec iter acc x = if x = 0 then acc else iter (10 * acc + (x mod 10)) (x / 10)
+  in iter 0 x
+
+let is_palindrom x = x = rev x
