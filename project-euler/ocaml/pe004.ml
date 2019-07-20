bfea8dee551b98f208b87674cff676cc872a5ee7
mitcc <mitcc@qq.com>
2019-07-21 03:25:34 +0800

    pe004.ml

--- project-euler/ocaml/pe004.ml
+++ project-euler/ocaml/pe004.ml
@@ -8,14 +8,14 @@
 open Math_lib
 
 let rec ff res a b i =
-  if a > b then res
-  else if Math_lib.is_palindrom (i * a) && (i * a) > res then ff (i * a) (a + 1) b i
-  else ff res (a + 1) b i
+  if a > b || (i * b) < res then res
+  else if Math_lib.is_palindrom (i * b) then ff (i * b) a (b - 1) i
+  else ff res a (b - 1) i
 
 let rec f res x y =
   if x > y then res
-  else if (ff res 100 999 x) > res then f (ff res 100 999 x) (x + 1) y
-  else f res (x + 1) y
+  else if (ff res 100 999 y) > res then f (ff res 100 999 y) x (y - 1)
+  else f res x (y - 1)
 
 let () =
   Format.printf "%d\n" (f 0 100 999)
